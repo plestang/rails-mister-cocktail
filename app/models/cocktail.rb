2f24aaa585f34521b40e22c69318497f81b4702b
Pierre Lestang <plestang@gmail.com>
2019-02-18 09:07:46 +0100

    ras

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,13 @@
 class Cocktail < ApplicationRecord
   mount_uploader :photo, PhotoUploader
-  validates :name, presence: true, uniqueness: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+
+  validates :name, presence: true, uniqueness: true
+  # validates :photo, si empty qalors tu execute tel methode
+
+
+  # def la_methode
+  #   self.photo.url = url de tq photo
+  # end
 end
